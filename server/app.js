bcc764e0a2a6e5e928d8601f02d20c86fd6b668a
Igor Boyko <iboyko@gmail.com>
2022-06-01 16:04:03 -0700

    Minor fix

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const { timeStamp } = require('console');
 const express = require('express');
-const fs = require('fs'); // file management
+const fs = require('fs');
 const csv = require('csvtojson');
 const app = express();
 
 app.use((req, res, next) => {
-// write your logging code here
+// logging code
 let agent = req.header("User-Agent").replace(/,/g, '');
 let time = new Date().toISOString();
 let method = req.method;
@@ -22,12 +22,12 @@ next();
 });
 
 app.get('/', (req, res) => {
-// write your code to respond "ok" here
+// respond "ok" to status code 200
 res.status(200).send("ok");
 });
 
 app.get('/logs', (req, res) => {
-// write your code to return a json object containing the log data here
+// return a json object containing the log data
     csv()
     .fromFile('log.csv')
     .then((jsonObj) => {
